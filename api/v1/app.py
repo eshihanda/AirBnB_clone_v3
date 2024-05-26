3f0690d28a801941a3d157165360be3bf55407b7
Pascal7601 <levispascal63@gmail.com>
2024-05-26 13:54:31 -0700

    my venv

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 create a flask api and mark its blueprint to an instance
 """
 
-from flask import Flask
+from flask import Flask, make_reponse
 from models import storage
 from os import getenv
 from api.v1.views import app_views
@@ -19,6 +19,12 @@ def teardown_db(self):
     """
     storage.close()
 
+@app.errorhandler(404)
+def not_found():
+    """
+    handles the default 404 request
+    """
+    return jsonify(make_response({"error": "Not found"}, 404))
 
 if __name__ == '__main__':
     host_vr = getenv('HBNB_API_HOST')
