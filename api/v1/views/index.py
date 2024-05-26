987269a1fb7492fb3b82031620b1bdef2be89331
eshihanda <eshihandamonn@gmail.com>
2024-05-26 13:34:46 -0700

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,27 @@ create route and status for the blueprint
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.review import Review
+from models.place import Place
+from models.user import User
 
 
 @app_views.route('/status')
 def status_api():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def some_stats():
+    return jsonify({
+        'amenities': storage.count(Amenity),
+        'cities': storage.count(City),
+        'places': storage.count(Place),
+        'reviews': storage.count(Review),
+        'states': storage.count(State),
+        'users': storage.count(User)
+        })
